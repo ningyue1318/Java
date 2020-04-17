525d6112a0cf67aeb79fbeb718bba566b20533a1
syn <1277323460@qq.com>
2020-04-17 16:50:58 +0800

    complete the linkedList

--- algorithm/src/LinkedList/SinglyLinkedList.java
+++ algorithm/src/LinkedList/SinglyLinkedList.java
@@ -71,6 +71,92 @@ public class SinglyLinkedList {
         System.out.println();
     }
 
+    public void deleteNode(Node p){
+        if(head==null||p==null){
+            return;
+        }
+
+        if(p==head){
+            head=null;
+            return;
+        }
+
+        Node q = head;
+        while(q!=null&&q.next!=p){
+            q=q.next;
+        }
+        if(q==null){
+            return;
+        }
+
+        q.next = p.next;
+
+    }
+    //判断true or false
+    public boolean TFResult(Node left, Node right){
+        Node l = left;
+        Node r = right;
+
+        boolean flag=true;
+        System.out.println("left_:"+l.data);
+        System.out.println("right_:"+r.data);
+        while(l != null && r != null){
+            if (l.data == r.data){
+                l = l.next;
+                r = r.next;
+                continue;
+            }else{
+                flag=false;
+                break;
+            }
+
+        }
+
+        System.out.println("什么结果");
+        return flag;
+    }
+
+    //无头结点的链表翻转
+    public Node inverseLinkList(Node p){
+      Node pre = null;
+      Node r = head;
+      Node next = null;
+      while(r!=p){
+          next = r.next;
+
+          r.next = pre;
+          pre = r;
+          r = next;
+      }
+      r.next =pre;
+      return r;
+    }
+
+    public boolean palindrome(){
+        if(head ==null){
+            return false;
+        }else{
+            Node pre = head;
+            if(pre.next==null){
+                return true;
+            }
+            Node after = pre.next.next;
+            if(after==null){
+                return false;
+            }
+            while(after!=null&&pre.data!=after.data){
+                after=after.next;
+                pre=pre.next;
+            }
+            if(after==null){
+                return false;
+            }else{
+                return TFResult(inverseLinkList(pre),after);
+            }
+        }
+    }
+
+
     public static Node createNode(int value){
         return new Node(value,null);
     }
@@ -91,11 +177,22 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedList link = new SinglyLinkedList();
+        /*
         link.insertToTail(1);
         link.insertToTail(2);
         link.insertToTail(3);
+        link.insertToTail(4);
+
+        Node p = link.inverseLinkList(link.findByValue(4));
+        link.head =p;
         link.printAll();
-        link.insertBefore(link.findByValue(2),new Node(4,null));
-        link.printAll();
+        */
+
+
+        int data [] ={1,2,5,2,1};
+        for(int i=0;i<data.length;i++){
+            link.insertToTail(data[i]);
+        }
+        System.out.println(link.palindrome());
     }
 }
