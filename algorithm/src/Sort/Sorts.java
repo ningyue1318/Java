d0acd72e7338de82ed57361b98cb5dc540b38c19
syn <1277323460@qq.com>
2020-04-23 15:14:54 +0800

    complete the mergeSort

--- algorithm/src/Sort/Sorts.java
+++ algorithm/src/Sort/Sorts.java
@@ -77,7 +77,7 @@ public class Sorts {
         quickSortInternally(a,0,n-1);
     }
 
-    public static void quickSortInternally(int [] a,int p,int r){
+    private static void quickSortInternally(int [] a,int p,int r){
         if(p>r) return;
 
         int q = partition(a,p,r);
@@ -106,13 +106,56 @@ public class Sorts {
     }
 
     /*
-     *堆排序
+     *归并排序
      */
+    public static void mergeSort(int [] a,int n){
+        mergeSortInternally(a,0,n-1);
+    }
+
+    private static void mergeSortInternally(int [] a,int p,int r){
+        if(p>=r) return ;
+
+        int q = p+(r-p)/2;
+
+        mergeSortInternally(a,p,q);
+        mergeSortInternally(a,q+1,r);
+
+        merge(a,p,q,r);
+    }
+
+    private static void merge(int [] a,int p,int q,int r){
+        int i = p;
+        int j = q+1;
+        int k = 0;
+        int [] tmp = new int[r-p+1];
+        while(i<=q&&j<=r){
+            if(a[i]<=a[j]){
+                tmp[k++] = a[i++];
+            }else{
+                tmp[k++] = a[j++];
+            }
+        }
+
+        int start = i;
+        int end = q;
+        if(j<=r){
+            start = j;
+            end = r;
+        }
+
+        while(start<=end){
+            tmp[k++] = a[start++];
+        }
+
+        for(i =0;i<=r-p;i++){
+            a[p+i]=tmp[i];
+        }
+    }
 
 
     public static void main(String[] args) {
         int [] array = new int[]{3,4,2,1,5,6,7,8};
-        quickSort(array,8);
+        mergeSort(array,8);
         System.out.println(Arrays.toString(array));
     }
 }
