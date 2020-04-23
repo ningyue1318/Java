fb6100b7274a8e6ae5c8085281ac0bc46579a916
syn <1277323460@qq.com>
2020-04-23 14:50:07 +0800

    complete the quicSort

--- algorithm/src/Sort/Sorts.java
+++ algorithm/src/Sort/Sorts.java
@@ -70,10 +70,49 @@ public class Sorts {
         }
     }
 
+    /*
+     * 快速排序
+     */
+    public static void quickSort(int [] a,int n){
+        quickSortInternally(a,0,n-1);
+    }
+
+    public static void quickSortInternally(int [] a,int p,int r){
+        if(p>r) return;
+
+        int q = partition(a,p,r);
+        quickSortInternally(a,p,q-1);
+        quickSortInternally(a,q+1,r);
+    }
+
+    private static int partition(int [] a,int p,int r){
+       int i = p;
+       int pivot = a[r];
+       for(int j=p;j<r;j++){
+           if(a[j]<pivot){
+               if(i==j){
+                   i++;
+               }else{
+                   int tmp = a[i];
+                   a[i++] = a[j];
+                   a[j] = tmp;
+               }
+           }
+       }
+       int tmp = a[i];
+       a[i] = a[r];
+       a[r] = tmp;
+       return i;
+    }
+
+    /*
+     *堆排序
+     */
+
 
     public static void main(String[] args) {
         int [] array = new int[]{3,4,2,1,5,6,7,8};
-        insertSort(array,8);
+        quickSort(array,8);
         System.out.println(Arrays.toString(array));
     }
 }
