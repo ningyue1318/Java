d9d71fd2bd55f8641952894ce6b6a61fd2bc0418
syn <1277323460@qq.com>
2020-05-25 10:55:37 +0800

    complete the BitMap

---
+++ algorithm/src/Array/SkipList.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class SkipList {
+    private static final float SKIPLIST_P = 0.5f;
+    private static final int MAX_LEVEL = 16;
+
+    private int levelCount = 1;
+
+    private  Node head = new Node();
+
+    public void printAll(){
+        Node p = head;
+        while(p.forwards[0]!=null){
+            System.out.println(p.forwards[0]+" ");
+            p = p.forwards[0];
+        }
+        System.out.println();
+    }
+
+    public class Node{
+        private int data = -1;
+        private Node forwards[] = new Node[MAX_LEVEL];
+        private int maxLevel = 0;
+
+        @Override
+        public String toString(){
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append("; levels: ");
+            builder.append(maxLevel);
+            builder.append(" }");
+
+            return builder.toString();
+        }
+    }
+}
---
+++ algorithm/src/thoughts/Dynamic2.java
@@ -0,0 +1,65 @@
+package thoughts;
+
+public class Dynamic2 {
+    private char [] a = "mitcmu".toCharArray();
+    private char [] b = "mtacnu".toCharArray();
+    private int n = 6;
+    private int m = 6;
+    private int minDist = Integer.MAX_VALUE;
+
+    public void lwstBT(int i,int j, int edist){
+        if(i==n||j==m){
+            if(i<n) edist += (n-i);
+            if(j<m) edist += (m-j);
+            if(edist<minDist) minDist = edist;
+            return;
+        }
+
+        if(a[i]==b[i]){
+            lwstBT(i+1,j+1,edist);
+        }else{
+            lwstBT(i+1,j,edist+1);
+            lwstBT(i,j+1,edist+1);
+            lwstBT(i+1,j+1,edist+1);
+        }
+    }
+
+    public int lwstDP(char [] a, int n,char [] b, int m){
+        int [][] minDist = new int [n][m];
+        for(int j=0;j<m;j++){
+            if(a[0]==b[j]) minDist[0][j]=j;
+            else if(j!=0) minDist[0][j] = minDist[0][j-1]+1;
+            else minDist[0][j] = 1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(a[i]==b[0]) minDist[i][0] = i;
+            else if(i!=0) minDist[i][0] = minDist[i-1][0]+1;
+            else minDist[i][0] =1;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(a[i]==b[j])
+                    minDist[i][j] = min(minDist[i-1][j]+1,minDist[i][j-1]+1,minDist[i-1][j-1]);
+                else
+                    minDist[i][j] = min(minDist[i-1][j]+1,minDist[i][j-1]+1,minDist[i-1][j-1]+1);
+            }
+        }
+        return minDist[n-1][n-1];
+    }
+
+    private int min(int x,int y, int z){
+        int minv = Integer.MAX_VALUE;
+        if(x<minv) minv = x;
+        if(y<minv) minv = y;
+        if(z<minv) minv = z;
+        return minv;
+    }
+
+    public static void main(String[] args) {
+        Dynamic2 d = new Dynamic2();
+        d.lwstBT(0,0,0);
+        System.out.println(d.lwstDP(d.a,d.n,d.b,d.m));
+    }
+}
